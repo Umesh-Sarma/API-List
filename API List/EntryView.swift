e034b3577c586e6f4f0f3b874e2906b40e5344b4
Umesh Sarma <usarma1200@gmail.com>
2024-03-08 09:48:48 -0600

    Setup EntryView

---
+++ API List/EntryView.swift
@@ -0,0 +1,18 @@
+//
+//  EntryView.swift
+//  API List
+//
+//  Created by Umesh Sarma on 3/5/24.
+//
+
+import SwiftUI
+
+struct EntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EntryView()
+}
